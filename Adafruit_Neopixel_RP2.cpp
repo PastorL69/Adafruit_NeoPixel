78f1954e3cc09559cea95773fe2d98cb688e2131
Jan Vos <jjcvos@hotmail.nl>
2026-06-17 22:51:41 +0200

    comment out sm as a test

--- Adafruit_Neopixel_RP2.cpp
+++ Adafruit_Neopixel_RP2.cpp
@@ -46,6 +46,6 @@ void  Adafruit_NeoPixel::rp2040Show(uint8_t *pixels, uint32_t numBytes)
 
   while(numBytes--)
     // Bits for transmission must be shifted to top 8 bits
-    pio_sm_put_blocking(pio, pio_sm, ((uint32_t)*pixels++)<< 24);
+    //pio_sm_put_blocking(pio, pio_sm, ((uint32_t)*pixels++)<< 24);
 }
 #endif
