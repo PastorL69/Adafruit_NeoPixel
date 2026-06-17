6a2d849e6c4b7b79edc20ac599bf8371241092f5
Jan Vos <jjcvos@hotmail.nl>
2026-06-17 22:55:55 +0200

    void pixels for now

--- Adafruit_Neopixel_RP2.cpp
+++ Adafruit_Neopixel_RP2.cpp
@@ -38,6 +38,7 @@ void Adafruit_NeoPixel::rp2040releasePIO(void) {
 
 // Private, called from show()
 void  Adafruit_NeoPixel::rp2040Show(uint8_t *pixels, uint32_t numBytes)
+void(*pixels)
 {
   // verify we have a valid PIO and state machine
   if (! pio || (pio_sm < 0)) {
@@ -46,6 +47,6 @@ void  Adafruit_NeoPixel::rp2040Show(uint8_t *pixels, uint32_t numBytes)
 
   while(numBytes--)
     // Bits for transmission must be shifted to top 8 bits
-    //pio_sm_put_blocking(pio, pio_sm, ((uint32_t)*pixels++)<< 24);
+    // pio_sm_put_blocking(pio, pio_sm, ((uint32_t)*pixels++)<< 24);
 }
 #endif
