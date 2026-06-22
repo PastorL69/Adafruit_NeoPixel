d82f49efe0bebdb148e3d3a84ac1eee0aa7cb04e
Jan Vos <jjcvos@hotmail.nl>
2026-06-23 00:58:21 +0200

    fix concurrency if destructed too many tmes back to back

--- Adafruit_Neopixel_RP2.cpp
+++ Adafruit_Neopixel_RP2.cpp
@@ -2,6 +2,8 @@
 
 #include "Adafruit_NeoPixel.h"
 
+bool waitNext = false; // blocking flag for a DMA transfer
+
 bool Adafruit_NeoPixel::rp2040claimPIO(void) {
   // Find a PIO with enough available space in its instruction memory
   pio = NULL;
@@ -59,6 +61,7 @@ void Adafruit_NeoPixel::rp2040releasePIO(void) {
   if (pio == NULL) 
     return;
 
+  skipNext = true;
   pio_remove_program_and_unclaim_sm(&ws2812_program, pio, pio_sm,  pio_program_offset);
 }
 
@@ -72,9 +75,12 @@ void Adafruit_NeoPixel::rp2040Show(uint8_t *pixels, uint32_t numBytes)
   }
 
   if(dma_chan >= 0) {
-    // set the read address and transfer immediately
-    dma_channel_set_read_addr(dma_chan, pixels, false);
-    dma_channel_set_trans_count(dma_chan, numBytes, true);
+    dma_channel_transfer_from_buffer_now(dma_chan, pixels, numBytes);
+    if (waitNext) {
+      // do not interrupt the current transfer.
+      waitNext = false;
+      dma_channel_wait_for_finish_blocking(dma_chan);
+    } 
     return;
   }
 
