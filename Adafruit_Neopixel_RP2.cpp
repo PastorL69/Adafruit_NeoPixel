51e875004cad6a52839ad60813329b08409477c3
Jan Vos <jjcvos@hotmail.nl>
2026-06-22 23:23:04 +0200

    should be tx

--- Adafruit_Neopixel_RP2.cpp
+++ Adafruit_Neopixel_RP2.cpp
@@ -60,7 +60,7 @@ void Adafruit_NeoPixel::rp2040releasePIO(void) {
   if (pio == NULL) 
     return;
 
-  while (!pio_sm_is_rx_fifo_empty(pio, pio_sm)) {
+  while (!pio_sm_is_tx_fifo_empty(pio, pio_sm)) {
     tight_loop_contents();
   }
   pio_remove_program_and_unclaim_sm(&ws2812_program, pio, pio_sm,  pio_program_offset);
