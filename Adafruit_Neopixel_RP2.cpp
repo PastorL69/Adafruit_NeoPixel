22a0ed7385d82b994d7682ae4fd9a2aa315e48a7
Jan Vos <jjcvos@hotmail.nl>
2026-06-23 22:16:09 +0200

    also clear fifo

--- Adafruit_Neopixel_RP2.cpp
+++ Adafruit_Neopixel_RP2.cpp
@@ -62,6 +62,7 @@ void Adafruit_NeoPixel::rp2040releasePIO(void) {
     return;
 
   dma_channel_wait_for_finish_blocking(dma_chan);
+  pio_sm_clear_fifos(pio, sm_pio);
   //blockNextDma = true; // the destructor occured, give the next DMA transfer breathing room.
                        // if not done, garbage data will enter the strip.
   pio_remove_program_and_unclaim_sm(&ws2812_program, pio, pio_sm,  pio_program_offset);
