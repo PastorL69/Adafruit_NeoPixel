afbca2ca78c0fe269960436af910e8eaba8ca3ea
Jan Vos <142336369+PastorL69@users.noreply.github.com>
2026-06-23 15:21:33 +0200

    Fix dma concurrency (#2)

--- Adafruit_Neopixel_RP2.cpp
+++ Adafruit_Neopixel_RP2.cpp
@@ -2,6 +2,8 @@
 
 #include "Adafruit_NeoPixel.h"
 
+bool blockNextDma = false; // blocking flag for a DMA transfer
+
 bool Adafruit_NeoPixel::rp2040claimPIO(void) {
   // Find a PIO with enough available space in its instruction memory
   pio = NULL;
@@ -59,6 +61,8 @@ void Adafruit_NeoPixel::rp2040releasePIO(void) {
   if (pio == NULL) 
     return;
 
+  blockNextDma = true; // the destructor occured, give the next DMA transfer breathing room.
+                       // if not done, garbage data will enter the strip.
   pio_remove_program_and_unclaim_sm(&ws2812_program, pio, pio_sm,  pio_program_offset);
 }
 
@@ -72,9 +76,12 @@ void Adafruit_NeoPixel::rp2040Show(uint8_t *pixels, uint32_t numBytes)
   }
 
   if(dma_chan >= 0) {
-    // set the read address and transfer immediately
-    dma_channel_set_read_addr(dma_chan, pixels, false);
-    dma_channel_set_trans_count(dma_chan, numBytes, true);
+    dma_channel_transfer_from_buffer_now(dma_chan, pixels, numBytes);
+    if (blockNextDma) {
+      // do not interrupt the current transfer.
+      blockNextDma = false;
+      dma_channel_wait_for_finish_blocking(dma_chan);
+    } 
     return;
   }
 
